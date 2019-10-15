a3cf2b4f7ff2d04fdd43d34f20818f80eb60a415
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-15 18:45:24 -0300

    Rotas de agendamento

---
+++ src/pages/New/Confirm/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Confirm() {
+  return <View />;
+}
+
+Confirm.navigationOptions = {
+  title: 'Confirme o agendamento',
+};
